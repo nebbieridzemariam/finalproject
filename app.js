f3d18bd8b4c98f2b2e86d1da9c99ca06025f206b
nebbieridzemariam <nebbieridze.mariam@gmail.com>
2022-02-15 22:21:17 +0400

    add 2nd slide

--- app.js
+++ app.js
@@ -13,3 +13,28 @@ function show(){
     slides[index-1].style.display="block";
     setTimeout(show, 3000);
 }
+
+// setTimeout(function start (){
+  
+//   $('.bar').each(function(i){  
+//     var $bar = $(this);
+//     $(this).append('<span class="count"></span>')
+//     setTimeout(function(){
+//       $bar.css('width', $bar.attr('data-percent'));      
+//     }, i*100);
+//   });
+ 
+// $('.count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).parent('.bar').attr('data-percent')
+//     }, {
+//         duration: 2000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now) +'%');
+//         }
+//     });
+// });
+ 
+// }, 500)
+// start ();
